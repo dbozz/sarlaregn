413f3065eb116ebe352ca7a537d204681feb6013
Zebastian Vidén <zebastian@gmail.com>
2024-10-06 20:41:19 +0200

    js

--- js/functions.js
+++ js/functions.js
@@ -147,7 +147,7 @@ function setTimestampCookie(cookieName) {
     var date = new Date();
     date.setTime(date.getTime() + (100 * 365 * 24 * 60 * 60 * 1000)); 
     var expires = "; expires=" + date.toUTCString();
-    document.cookie = cookieName + "=" + timestamp + expires + "; path=/;";
+    document.cookie = cookieName + "=" + timestamp + expires + "; path=/; SameSite=None; Secure";
 }
 
 function ajaxSR(key1) {
