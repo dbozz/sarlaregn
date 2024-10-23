f394d92319a181ea40498c3c5da1f31323f05a54
Zebastian Vidén <zebastian@gmail.com>
2024-10-23 17:07:01 +0200

    Uppdaterad js

--- js/functions.js
+++ js/functions.js
@@ -190,7 +190,8 @@ function updateDbVersionCookie() {
 
 function ajaxSR(key1) {
     return new Dexie.Promise(function (resolve, reject) {
-        $.ajax("/sr.json", {
+        var now = Math.floor(Date.now() / 1000);
+        $.ajax("/sr.json?t=" + now, {
             type: 'get',
             dataType: 'json',
             error: function (xhr, textStatus) {
