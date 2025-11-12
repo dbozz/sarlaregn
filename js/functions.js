256b406d66260d8cf90b168c3b7446ca55b6b41b
Zebastian Vidén <zebastianviden@MacBook-Air-som-tillhor-Zebastian.local>
2025-11-12 11:50:26 +0100

    new db

--- js/functions.js
+++ js/functions.js
@@ -265,6 +265,7 @@ function ajaxSR(key1) {
                         return;  // Skip this item
                     }
                 }*/
+                console.log("Inserting item with id: ", item.id);
                 db.lyrics.add(item);
             });
         });
